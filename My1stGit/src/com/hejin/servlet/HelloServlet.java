3a967289d89cded7619f320bdecd88fb0ea5a5cc
hejin666 <hejin991@sohu.com>
2018-06-15 09:30:16 +0800

    修改了HelloServlet， 新增了UserServlet

--- My1stGit/src/com/hejin/servlet/HelloServlet.java
+++ My1stGit/src/com/hejin/servlet/HelloServlet.java
@@ -26,7 +26,7 @@ public class HelloServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub//修改了
+		// TODO Auto-generated method stub//修改了，又改了
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
